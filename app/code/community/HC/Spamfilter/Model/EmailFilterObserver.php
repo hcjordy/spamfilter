f432fabceef37b47b2985d0e06ae4061d4c97aec
Martijn Warrink <mwarrink@hcdesigns.nl>
2018-04-05 13:03:37 +0200

    Add filter for contact form

--- app/code/community/HC/Spamfilter/Model/EmailFilterObserver.php
+++ app/code/community/HC/Spamfilter/Model/EmailFilterObserver.php
@@ -25,28 +25,30 @@ class HC_Spamfilter_Model_EmailFilterObserver {
         if (!$this->checkEmail($request->getPost('email')) || !$this->checkText($request->getPost('email'))) {
             $this->session()->addError($this->helper()->__('You\'re not allowed to subscribe'));
             $url = Mage::helper('core/http')->getHttpReferer() ? Mage::helper('core/http')->getHttpReferer()  : Mage::getUrl();
+
             $subscriber->getResponse()->setRedirect($url);
             $subscriber->getResponse()->sendResponse();
-
             exit(1);
         }
 
     }
 
-    /**
-     * @return Mage_Customer_Model_Session
-     */
-    private function session()
+    public function checkContactsForm(Varien_Event_Observer $observer)
     {
-        return Mage::getSingleton('customer/session');
-    }
+        $contact = $observer->getControllerAction();
+        $request = $contact->getRequest();
+        if (is_null($request->getPost('email'))) {
+            return false;
+        }
 
-    /**
-     * @return HC_Spamfilter_Helper_Data
-     */
-    private function helper()
-    {
-        return Mage::helper('spamfilter');
+        if (!$this->checkEmail($request->getPost('email')) || !$this->checkText($request->getPost('email'), $request->getPost('name'), $request->getPost('comment'), $request->getPost('telephone'))) {
+            $this->session()->addError($this->helper()->__('You\'re not allowed to contact us'));
+            $url = Mage::helper('core/http')->getHttpReferer() ? Mage::helper('core/http')->getHttpReferer()  : Mage::getUrl();
+
+            $contact->getResponse()->setRedirect($url);
+            $contact->getResponse()->sendResponse();
+            exit(1);
+        }
     }
 
 
@@ -62,16 +64,15 @@ class HC_Spamfilter_Model_EmailFilterObserver {
             return false;
         }
         if (!$this->checkEmail($request->getPost('email')) || !$this->checkText($request->getPost('email'), $request->getPost('firstname'), $request->getPost('lastname'))) {
-            die('FOUND!');
             $request->setPost('email', '');
             $session = Mage::getSingleton('customer/session');
             $session->addError($this->helper()->__('You\'re not allowed to create an account'));
             $url = Mage::helper('core/http')->getHttpReferer() ? Mage::helper('core/http')->getHttpReferer()  : Mage::getUrl();
+
             $account->getResponse()->setRedirect($url);
             $account->getResponse()->sendResponse();
             exit(1);
         }
-        die('NOT FOUND!');
 
     }
 
@@ -100,6 +101,9 @@ class HC_Spamfilter_Model_EmailFilterObserver {
     {
         $blockedText = explode(",", strtolower(Mage::getStoreConfig(self::TEXT_BLOCK_SETTING_PATH)));
         foreach($text as $item) {
+            if (empty($item)) {
+                continue;
+            }
             foreach ($blockedText as $block) {
 
                 if (empty($block)) {
@@ -107,10 +111,28 @@ class HC_Spamfilter_Model_EmailFilterObserver {
                 }
 
                 if (stripos($item, trim($block)) !== false) {
-//                    return false;
+                    return false;
                 }
             }
         }
         return false;
+    }
+
+
+
+    /**
+     * @return Mage_Customer_Model_Session
+     */
+    private function session()
+    {
+        return Mage::getSingleton('customer/session');
+    }
+
+    /**
+     * @return HC_Spamfilter_Helper_Data
+     */
+    private function helper()
+    {
+        return Mage::helper('spamfilter');
     }
 }
